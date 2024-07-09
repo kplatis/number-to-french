ca3a0242353d571d04b166ce390a5d331b706c71
kplatis <kplatis@protonmail.com>
2024-07-09 20:55:40 +0200

    Add main implementation with test cases

--- number_to_french.py
+++ number_to_french.py
@@ -30,35 +30,82 @@ class NumberToFrench:
             5: "cinquante",
             6: "soixante",
         }
+        self.special_words = {
+            1: "mille",
+            2: "millions",
+        }
+
         self.number = number
 
-    def number_to_word(self, num: int) -> str:
+    def triple_digit_to_word(self, num: int) -> str | None:
+        """
+        Convert triplets of numbers to words
+        """
+        str_num = str(num)
+        # if its less than 3 digits or if the number starts with 0
+        if len(str_num) < 3 or str_num[0] == "0":
+            return self.double_digit_to_word(int(str_num))
+        else:
+            first_digit, rest = str_num[0], str_num[1:3]
+            if first_digit == 1:
+                return f"cent {self.double_digit_to_word(int(rest))}"
+            else:
+                return f"{self.digits_to_words[int(first_digit)]} cent {self.double_digit_to_word(int(rest))}"
+
+    def double_digit_to_word(self, num: int) -> str | None:
         """
         Function that receives a number up to 2 digits and turns it to a word string
         """
-        if num < 16:
+        if num == 0:
+            return None
+        if num < 19:
             return self.digits_to_words[num]
         else:
             first_num, second_num = int(str(num)[0]), int(str(num)[1])
-            # 17-69
+            # 20-69
             if num < 70:
                 if second_num == 0:
                     return self.tens[first_num]
-                # special case where number has 1 as the second number => need to append "et"
                 elif second_num == 1:
                     return f"{self.tens[first_num]}-et-un"
                 else:
                     return f"{self.tens[first_num]}-{self.digits_to_words[second_num]}"
             # 70-79
             elif num < 80:
-                return f"soixante-{self.digits_to_words[10+second_num]}"
-            # 80-89
-            elif num < 90:
-                if second_num == 0:
-                    return "quatre-vingt"
-                elif second_num == 1:
-                    return "quatre-vingt-et-un"
-                else:
-                    return f"quatre-vingt-{self.digits_to_words[second_num]}"
+                return f"soixante-{self.digits_to_words[10 + second_num]}"
+            # 80 +
             else:
-                return f"quatre-vingt-{self.digits_to_words[10+second_num]}"
+                prefix = "quatre-vingt"
+                if num < 90:
+                    if second_num == 0:
+                        return prefix
+                    elif second_num == 1:
+                        return f"{prefix}-et-un"
+                    else:
+                        return f"{prefix}-{self.digits_to_words[second_num]}"
+                else:
+                    return f"{prefix}-{self.digits_to_words[10 + second_num]}"
+
+    def to_french_word(self) -> str:
+        """
+        Splits the word in chunks of three, then converts each chunk of three numbers to words.
+
+        In any chunk apart from the first one appends special words (million, mille etc)
+        """
+        # convert the number to string for easier parsing
+        str_num = str(self.number)[::-1]
+        if len(str_num) > 9:
+            raise Exception("Supported numbers up to 9 digits")
+        words = []
+        # split the word in chunks of 3
+        chunks = [str_num[i : i + 3] for i in range(0, len(str_num), 3)]
+        # Reverse each chunk and then reverse the list of chunks
+        reversed_chunks = [chunk[::-1] for chunk in chunks]
+        # iterate chunks and build the word
+        for idx, chunk in enumerate(reversed_chunks):
+            if idx in self.special_words:
+                words.append(self.special_words[idx])
+            word = self.triple_digit_to_word(int(chunk))
+            if word:
+                words.append(word)
+        return " ".join(reversed(words))
--- tests/test_number_to_french.py
+++ tests/test_number_to_french.py
@@ -3,23 +3,54 @@ from number_to_french import NumberToFrench
 
 def test_number_to_word():
 
-    result = NumberToFrench(0).number_to_word(4)
+    result = NumberToFrench(0).double_digit_to_word(4)
     assert result == "quatre"
-    result = NumberToFrench(0).number_to_word(17)
+    result = NumberToFrench(0).double_digit_to_word(17)
     assert result == "dix-sept"
-    result = NumberToFrench(0).number_to_word(30)
+    result = NumberToFrench(0).double_digit_to_word(30)
     assert result == "trente"
-    result = NumberToFrench(0).number_to_word(51)
+    result = NumberToFrench(0).double_digit_to_word(51)
     assert result == "cinquante-et-un"
-    result = NumberToFrench(0).number_to_word(63)
+    result = NumberToFrench(0).double_digit_to_word(63)
     assert result == "soixante-trois"
-    result = NumberToFrench(0).number_to_word(73)
+    result = NumberToFrench(0).double_digit_to_word(73)
     assert result == "soixante-treize"
-    result = NumberToFrench(0).number_to_word(80)
+    result = NumberToFrench(0).double_digit_to_word(80)
     assert result == "quatre-vingt"
-    result = NumberToFrench(0).number_to_word(86)
+    result = NumberToFrench(0).double_digit_to_word(86)
     assert result == "quatre-vingt-six"
-    result = NumberToFrench(0).number_to_word(90)
+    result = NumberToFrench(0).double_digit_to_word(90)
     assert result == "quatre-vingt-dix"
-    result = NumberToFrench(0).number_to_word(99)
+    result = NumberToFrench(0).double_digit_to_word(99)
     assert result == "quatre-vingt-dix-neuf"
+
+
+def test_triple_digit_to_word():
+    result = NumberToFrench(0).triple_digit_to_word(305)
+    assert result == "trois cent cinq"
+    result = NumberToFrench(0).triple_digit_to_word(888)
+    assert result == "huit cent quatre-vingt-huit"
+
+
+def test_build_word():
+    result = NumberToFrench(9).to_french_word()
+    assert result == "neuf"
+    result = NumberToFrench(16).to_french_word()
+    assert result == "seize"
+    result = NumberToFrench(105).to_french_word()
+    assert result == "un cent cinq"
+    result = NumberToFrench(10000).to_french_word()
+    assert result == "dix mille"
+    result = NumberToFrench(1653).to_french_word()
+    assert result == "un mille six cent cinquante-trois"
+    result = NumberToFrench(5135000).to_french_word()
+    assert result == "cinq millions un cent trente-cinq mille"
+    result = NumberToFrench(1005321).to_french_word()
+    assert result == "un millions cinq mille trois cent vingt-et-un"
+    result = NumberToFrench(289005321).to_french_word()
+    assert (
+        result
+        == "deux cent quatre-vingt-neuf millions cinq mille trois cent vingt-et-un"
+    )
+    result = NumberToFrench(25021).to_french_word()
+    assert result == "vingt-cinq mille vingt-et-un"
